55b5d5bd236be619012f606840e52a308e42839e
thomas70 <thomas@longva.com>
2025-07-17 09:59:09 +0200

    Update TrumaRoomClimate.h
    Esp home required changes 2025.11.0

--- components/truma_inetbox/climate/TrumaRoomClimate.h
+++ components/truma_inetbox/climate/TrumaRoomClimate.h
@@ -1,22 +1,33 @@
 #pragma once
 
 #include "esphome/components/climate/climate.h"
+#include "esphome/core/component.h"
 #include "esphome/components/truma_inetbox/TrumaiNetBoxApp.h"
 
 namespace esphome {
-namespace truma_inetbox {
-class TrumaRoomClimate : public Component, public climate::Climate, public Parented<TrumaiNetBoxApp> {
- public:
-  void setup() override;
+  namespace truma_inetbox {
+  
+  class TrumaRoomClimate : public Component, public climate::Climate, public Parented<TrumaiNetBoxApp> {
+   public:
+    void loop() override {}
+    void dump_config() override;
+    void control(const climate::ClimateCall &call) override;
+    void setup() override;
+    climate::ClimateTraits traits() override;
+  
+  
+    void set_visual_min_temperature(float value) { this->visual_min_temperature_ = value; }
+    void set_visual_max_temperature(float value) { this->visual_max_temperature_ = value; }
+    void set_visual_temperature_step(float value) { this->visual_temperature_step_ = value; }
 
-  void dump_config() override;
-
-  void control(const climate::ClimateCall &call) override;
-
-  climate::ClimateTraits traits() override;
-
- protected:
- private:
-};
-}  // namespace truma_inetbox
-}  // namespace esphome
+    void set_supported_modes(const std::set<climate::ClimateMode> &modes);
+  
+   protected:
+    std::set<esphome::climate::ClimateMode> supported_modes_;
+    float visual_min_temperature_{5.0};
+    float visual_max_temperature_{30.0};
+    float visual_temperature_step_{0.5};
+  };
+  
+  }  // namespace truma_inetbox
+  }  // namespace esphome
